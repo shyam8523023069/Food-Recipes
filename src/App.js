c76efecaf6a5cec3cf315644534644cab06d16d7
shyam <shyammanideep1997@gmail.com>
2021-01-08 22:51:23 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { RecipesState } from './context/RecipesContext';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Nav from './components/Nav';
 import './App.css';
+import Hero from './components/Hero';
+import FullRecipe from "./components/FullRecipe";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecipesState>
+      <Router>
+        <Nav />
+        <Route exact path="/" component={Hero} />
+        <Route path="/recipes/:id" component={FullRecipe} />
+      </Router>
+    </RecipesState>
+
+
   );
-}
+};
 
 export default App;
