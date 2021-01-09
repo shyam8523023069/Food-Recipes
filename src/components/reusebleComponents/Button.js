5b079f215faff7126fc8f6eb5c2150709923354f
shyam <shyammanideep1997@gmail.com>
2021-01-09 22:52:57 +0530

    comp

---
+++ src/components/reusebleComponents/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Button =({btnText, handleClick})=> {
+    return(
+<button className='btn' onClick={handleClick} >{btnText}</button>
+)}
+
+export default Button;
